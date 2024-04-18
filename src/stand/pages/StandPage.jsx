c2e0a3a8ab573f22e1e0aa0c7bbb5d320e389cb8
Grober Gonzales <GonzalesDLCGrober@gmail.com>
2024-04-18 16:48:16 -0500

    Disign

--- src/stand/pages/StandPage.jsx
+++ src/stand/pages/StandPage.jsx
@@ -3,6 +3,7 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { startLoadingProducts } from '../../store/stand/thunks';
 import { Product } from '../../ui/Product';
+import { Grid } from '@mui/material';
 
 export const StandPage = () => {
   const { products } = useSelector((state) => state.stand);
@@ -14,9 +15,11 @@ export const StandPage = () => {
 
   return (
     <StandLayout>
-      {products.map((product) => (
-        <Product key={product.id} product={product} />
-      ))}
+      <Grid container spacing={2} sx={{ padding: 1 }}>
+        {products.map((product) => (
+          <Product key={product.id} product={product} />
+        ))}
+      </Grid>
     </StandLayout>
   );
 };
--- src/ui/Product.jsx
+++ src/ui/Product.jsx
@@ -1,19 +1,94 @@
-import { Box, List, Typography } from '@mui/material';
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  Grid,
+  IconButton,
+  Typography,
+} from '@mui/material';
 import PropTypes from 'prop-types';
 import { productImageLink } from '../helpers/productImageLink';
+import { useState } from 'react';
+import {
+  ArrowLeft,
+  ArrowRight,
+  LocalGroceryStoreOutlined,
+  WhatsApp,
+} from '@mui/icons-material';
 
 export const Product = ({ product: { nombre, precio, marca, fotos } }) => {
+  const [iImg, setIImg] = useState(0);
+
+  const nextImage = () => {
+    if (iImg + 1 < fotos.length) {
+      setIImg(iImg + 1);
+    } else {
+      setIImg(0);
+    }
+  };
+  const prevImage = () => {
+    if (iImg > 0) {
+      setIImg(iImg - 1);
+    } else {
+      setIImg(fotos.length - 1);
+    }
+  };
+
   return (
-    <Box>
-      <Typography>{nombre}</Typography>
-      <Typography>{marca}</Typography>
-      <Typography>{precio}</Typography>
-      <List>
-        {fotos.map((url) => (
-          <img key={url} src={productImageLink(url)} />
-        ))}
-      </List>
-    </Box>
+    <Grid item xs={12} sm={6} md={3}>
+      <Card sx={{ bgcolor: '#efefef' }}>
+        <CardHeader title={nombre} subheader={marca} />
+        <Box position='relative'>
+          <CardMedia
+            component='img'
+            height={256}
+            image={productImageLink(fotos[iImg])}
+            alt={nombre}
+          />
+          <Box
+            position='absolute'
+            sx={{
+              width: '100%',
+              top: 0,
+              left: 0,
+              display: fotos.length >= 2 ? 'flex' : 'none',
+              justifyContent: 'space-between',
+            }}
+          >
+            <Button variant='contained' color='primary' onClick={prevImage}>
+              <ArrowLeft />
+            </Button>
+            <Typography
+              sx={{
+                bgcolor: '#fff4',
+                borderRadius: 4,
+                padding: 1,
+              }}
+            >
+              {iImg + 1} / {fotos.length}
+            </Typography>
+            <Button variant='contained' color='primary' onClick={nextImage}>
+              <ArrowRight />
+            </Button>
+          </Box>
+        </Box>
+        <CardContent>
+          <Typography>S/. {precio}</Typography>
+        </CardContent>
+        <CardActions>
+          <IconButton>
+            <LocalGroceryStoreOutlined />
+          </IconButton>
+          <IconButton>
+            <WhatsApp />
+          </IconButton>
+        </CardActions>
+      </Card>
+    </Grid>
   );
 };
 
